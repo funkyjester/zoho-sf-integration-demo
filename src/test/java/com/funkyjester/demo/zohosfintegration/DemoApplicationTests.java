e44493bccfd126087ca4dc77317df4533731c240
Gabriel Chan <code@gabrielchan.ca>
2021-03-06 08:48:59 -0800

    initial spring init

---
+++ src/test/java/com/funkyjester/demo/zohosfintegration/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funkyjester.demo.zohosfintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
